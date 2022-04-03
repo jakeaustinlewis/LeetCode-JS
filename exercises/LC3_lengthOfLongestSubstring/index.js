f5b99317fcfd2fabdcb2ae64d374f1fc7abf8d20
Jake <jakelewis@Jakes-MBP.lan>
2022-04-03 16:33:12 -0500

    length of longest substring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,44 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+
+// Create an empty hashMap (key/val -> character/index)
+// Create start and max variable, set both with initial values of zero
+
+// Loop thru input string
+//  If current Character in hashmap & has index >= start
+//    Set start to index for character found in hasmap + 1
+// Set key/value pair on hashmap to be current character/current index
+// if length of current window is greater than max
+//  Set max to length of current window
+
+
+function lengthOfLongestSubstring(s) {
+  let windowCharsMap = {}; // map letters as keys to its index
+  let windowStart = 0;
+  let maxLength = 0;
+
+  // windowCharsMap = {
+  //   a = 5,
+  //   b = 0,
+  //   c = 2
+  // }
+
+  for (let i = 0; i < s.length; i++) {
+    const endChar = s[i];
+
+    if (windowCharsMap[endChar] >= windowStart) {
+      windowStart = windowCharsMap[endChar] + 1;
+    }
+
+    // reassign key to new value (index)
+    windowCharsMap[endChar] = i;
+
+    if (maxLength < i - windowStart + 1) {
+      maxLength = i - windowStart + 1;
+    }
+  }
+  return maxLength;
+}
 
 module.exports = lengthOfLongestSubstring;
--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,29 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, '');
+  console.log('s: ', s);
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandAroundMiddle(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        maxLength = currentPalLength;
+        startIndex = left;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundMiddle(i - 1, i + 1); // odd characters example : aba
+    expandAroundMiddle(i, i + 1); // even characters example : abba
+  }
+  return s.slice(startIndex, startIndex + maxLength);
+}
 
 module.exports = longestPalindrome;
