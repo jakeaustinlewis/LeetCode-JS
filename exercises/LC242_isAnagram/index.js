f9583d60821f468dd110a48ab93c4336b4af0434
Jake <jakelewis@Jakes-MBP.lan>
2022-04-03 17:19:30 -0500

    isAnagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,35 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const sCharsCounts = {}; // { H: 1, E: 1, L: 2, O: 1 }
+  const tCharsCounts = {}; // { H: 1, E: 1, L: 2, O: 1 }
+
+
+  for (let i = 0; i < s.length; i++) {
+    let sEndChar = s[i];
+    let tEndChar = t[i];
+    sCharsCounts[sEndChar] = sCharsCounts[sEndChar] + 1 || 1;
+    tCharsCounts[tEndChar] = tCharsCounts[tEndChar] + 1 || 1;
+  }
+
+  return Object.keys(sCharsCounts).every((char, index) => {
+    return sCharsCounts[char] === tCharsCounts[char];
+  })
+
+
+  // One solution
+  // s = s.split('').sort().join();
+  // t = t.split('').sort().join();
+  // return t === s;
+
+
+  // another solution
+  // s = s.split('').sort();
+  // t = t.split('').sort();
+  // return s.length === t.length && s.every((sElement, index) => {
+  //   return sElement === t[index];
+  // })
+
+  // One last solution
+}
 
 module.exports = isAnagram;
--- javascript-refersher/index.js
+++ javascript-refersher/index.js
@@ -19,8 +19,22 @@ function addNums(a, b) {
 
 addNums(5, 10);
 
+
+
+
+// split() -> string to array
+// join('') -> array to string
+
 // The splice() method changes the contents of an array by removing or replacing
 // existing elements and/or adding new elements in place. To access part of an array
 // without modifying it, see slice()
 const months = ['Jan', 'March', 'April', 'June'];
 months.splice(1, 0, 'Feb');
+
+// every() tests whether all elements in the array pass the test implemented by
+// the provided function. It returns a Boolean value.
+
+// some()  tests whether at least one element in the array passes the test
+// implemented by the provided function. It returns true if, in the array, it
+// finds an element for which the provided function returns true; otherwise it
+// returns false. It doesn't modify the array. It returns a Boolean value.
